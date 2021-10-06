81a6add6a354a0d795db6b289025ff593d1ec493
NewPirateOfUASeas <75931217+NewPirateOfUASeas@users.noreply.github.com>
2021-10-06 22:31:11 +0300

    prettify

--- src/components/AuthModal.vue
+++ src/components/AuthModal.vue
@@ -1,89 +1,68 @@
 <template>
-  <div
-    v-show="authModalShowed"
-    class="fixed z-10 inset-0 overflow-y-auto"
-    id="modal"
-  >
-    <div
-      class="
-        flex
-        items-end
-        justify-center
-        min-h-screen
-        pt-4
-        px-4
-        pb-20
-        text-center
-        sm:block sm:p-0
-      "
-    >
-      <div class="fixed inset-0 transition-opacity">
-        <div class="absolute inset-0 bg-gray-800 opacity-75"></div>
-      </div>
+	<div v-show="authModalShowed" class="fixed z-10 inset-0 overflow-y-auto" id="modal">
+		<div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+			<div class="fixed inset-0 transition-opacity">
+				<div class="absolute inset-0 bg-gray-800 opacity-75"></div>
+			</div>
 
-      <!-- This element is to trick the browser into centering the modal contents. -->
-      <span class="hidden sm:inline-block sm:align-middle sm:h-screen"
-        >&#8203;</span
-      >
+			<!-- This element is to trick the browser into centering the modal contents. -->
+			<span class="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
 
-      <div
-        class="
-          inline-block
-          align-bottom
-          bg-white
-          rounded-lg
-          text-left
-          overflow-hidden
-          shadow-xl
-          transform
-          transition-all
-          sm:my-8 sm:align-middle sm:max-w-lg sm:w-full
-        "
-      >
-        <!-- Add margin if you want to see some of the overlay behind the modal-->
-        <div class="py-4 text-left px-6">
-          <!--Title-->
-          <div class="flex justify-between items-center pb-4">
-            <p class="text-2xl font-bold">Your Account</p>
-            <!-- Modal Close Button -->
-            <div
-              class="modal-close cursor-pointer z-50"
-              @click="toggleAuthModal"
-            >
-              <i class="fas fa-times"></i>
-            </div>
-          </div>
+			<div
+				class="
+					inline-block
+					align-bottom
+					bg-white
+					rounded-lg
+					text-left
+					overflow-hidden
+					shadow-xl
+					transform
+					transition-all
+					sm:my-8 sm:align-middle sm:max-w-lg sm:w-full
+				"
+			>
+				<!-- Add margin if you want to see some of the overlay behind the modal-->
+				<div class="py-4 text-left px-6">
+					<!--Title-->
+					<div class="flex justify-between items-center pb-4">
+						<p class="text-2xl font-bold">Your Account</p>
+						<!-- Modal Close Button -->
+						<div class="modal-close cursor-pointer z-50" @click="toggleAuthModal">
+							<i class="fas fa-times"></i>
+						</div>
+					</div>
 
-          <!-- Tabs -->
-          <ul class="flex flex-wrap mb-4">
-            <li class="flex-auto text-center">
-              <a
-                class="block rounded py-3 px-4 transition"
-                :class="isLogIn ? activeClass : nonActiveClass"
-                href="#"
-                @click.prevent="isLogIn = true"
-                >Login</a
-              >
-            </li>
-            <li class="flex-auto text-center">
-              <a
-                class="block rounded py-3 px-4 transition"
-                :class="isLogIn ? nonActiveClass : activeClass"
-                href="#"
-                @click.prevent="isLogIn = false"
-                >Register</a
-              >
-            </li>
-          </ul>
+					<!-- Tabs -->
+					<ul class="flex flex-wrap mb-4">
+						<li class="flex-auto text-center">
+							<a
+								class="block rounded py-3 px-4 transition"
+								:class="isLogIn ? activeClass : nonActiveClass"
+								href="#"
+								@click.prevent="isLogIn = true"
+								>Login</a
+							>
+						</li>
+						<li class="flex-auto text-center">
+							<a
+								class="block rounded py-3 px-4 transition"
+								:class="isLogIn ? nonActiveClass : activeClass"
+								href="#"
+								@click.prevent="isLogIn = false"
+								>Register</a
+							>
+						</li>
+					</ul>
 
-          <!-- isLoggedIn Form -->
-          <login-form v-show="isLogIn" />
-          <!-- Registration Form -->
-          <registr-form v-show="!isLogIn" />
-        </div>
-      </div>
-    </div>
-  </div>
+					<!-- isLoggedIn Form -->
+					<login-form v-show="isLogIn" />
+					<!-- Registration Form -->
+					<registr-form v-show="!isLogIn" />
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -91,25 +70,25 @@ import LoginForm from "./LoginForm.vue";
 import RegistrForm from "./RegistrForm.vue";
 
 export default {
-  components: { LoginForm, RegistrForm },
-  name: "Auth",
+	components: { LoginForm, RegistrForm },
+	name: "Auth",
 
-  data() {
-    return {
-      activeClass: "hover:text-white text-white bg-blue-600",
-      nonActiveClass: "hover:text-blue-600",
-      isLogIn: true, // if isLoggedIn === true than we're on isLoggedIn page, else we're on register pag
-    };
-  },
-  computed: {
-    authModalShowed() {
-      return this.$store.state.authModalShowed;
-    },
-  },
-  methods: {
-    toggleAuthModal() {
-      this.$store.commit("toggleAuthModal");
-    },
-  },
+	data() {
+		return {
+			activeClass: "hover:text-white text-white bg-blue-600",
+			nonActiveClass: "hover:text-blue-600",
+			isLogIn: true, // if isLoggedIn === true than we're on isLoggedIn page, else we're on register pag
+		};
+	},
+	computed: {
+		authModalShowed() {
+			return this.$store.state.authModalShowed;
+		},
+	},
+	methods: {
+		toggleAuthModal() {
+			this.$store.commit("toggleAuthModal");
+		},
+	},
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,39 +3,38 @@ import { addUsersCollection } from "@/includes/firebase.js";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 export default createStore({
-  state: {
-    authModalShowed: false,
-    userLoggedIn: false,
-  },
-  mutations: {
-    toggleAuthModal(state) {
-      state.authModalShowed = !state.authModalShowed;
-    },
-    toggleAuth(state) {
-      state.userLoggedIn = !state.userLoggedIn;
-    },
-  },
-  getters: {},
-  actions: {
-    async register({ commit }, payload) {
-      let userCredentials
-      const auth = getAuth();
-      await createUserWithEmailAndPassword(
-        auth,
-        payload.email,
-        payload.password
-      ).then((data) => {
-        userCredentials = data;
-      });
-      await addUsersCollection({
-        name: payload.name,
-        email: payload.email,
-        age: payload.age,
-        country: payload.country,
-      }, userCredentials.user.uid);
+	state: {
+		authModalShowed: false,
+		userLoggedIn: false,
+	},
+	mutations: {
+		toggleAuthModal(state) {
+			state.authModalShowed = !state.authModalShowed;
+		},
+		toggleAuth(state) {
+			state.userLoggedIn = !state.userLoggedIn;
+		},
+	},
+	getters: {},
+	actions: {
+		async register({ commit }, payload) {
+			let userCredentials;
+			const auth = getAuth();
+			await createUserWithEmailAndPassword(auth, payload.email, payload.password).then((data) => {
+				userCredentials = data;
+			});
+			await addUsersCollection(
+				{
+					name: payload.name,
+					email: payload.email,
+					age: payload.age,
+					country: payload.country,
+				},
+				userCredentials.user.uid
+			);
 
-      commit ('toggleAuth')
-    },
-  },
-  modules: {},
+			commit("toggleAuth");
+		},
+	},
+	modules: {},
 });
