578247cc51d8034a925481be076550741ed9750d
NewPirateOfUASeas <75931217+NewPirateOfUASeas@users.noreply.github.com>
2021-10-16 19:59:44 +0300

    everything was more or less understandable before this commit. Now it drastically differs from courses codebase

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -2,7 +2,7 @@
 	<section class="container mx-auto mt-6">
 		<div class="md:grid md:grid-cols-3 md:gap-4">
 			<div class="col-span-1">
-				<file-uploader :songData="songData" />
+				<file-uploader :songData="songData" :addSong="addSong" />
 			</div>
 			<div class="col-span-2">
 				<div class="bg-white rounded border border-gray-200 relative flex flex-col">
@@ -12,7 +12,16 @@
 					</div>
 					<div class="p-6">
 						<!-- Composition Items -->
-						<composition-item v-for="song in songs" :song="song" :key="song.docID" @submit="submit" />
+						<composition-item
+							v-for="(song, index) in songs"
+							:song="song"
+							:key="song.docID"
+							:index="index"
+							:updateSong="updateSong"
+							@submit="submit"
+						/>
+						<!-- It's considered to be a bad practice to pass functions through props. 
+						Made only for demonstration purposes  -->
 					</div>
 				</div>
 			</div>
@@ -38,17 +47,21 @@ export default {
 	},
 	async created() {
 		const snapshot = await getSongCollection();
-		snapshot.forEach((doc) => {
+		snapshot.forEach(this.addSong);
+	},
+	methods: {
+		updateSong(index, values) {
+			this.songs[index].modified_name = values.modified_name;
+			this.songs[index].genre = values.genre;
+		},
+		addSong(doc) {
 			const song = {
 				...doc.data(),
 				docID: doc.id,
 			};
 			console.log(song);
 			this.songs.push(song);
-		});
-	},
-	methods: {
-		submit() {},
+		},
 	},
 };
 </script>
